48a326f09e60cfad4210650c532a4157f90fff11
Mahdi Badari <mahdi.badari@mitrais.com>
2018-07-03 09:52:13 +0700

    update with spring JMS

---
+++ springboot-tutor/src/main/java/com/mycompany/app/Receiver.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.springframework.jms.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
+    public void receiveMessage(Email email) {
+        System.out.println("Received <" + email + ">");
+    }
+
+}
